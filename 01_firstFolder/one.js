21de7144c1d1313cd20bba67aabb8f8e71273850
Wasim2030 <137741061+Wasim2030@users.noreply.github.com>
2023-12-07 05:15:40 +0000

    basic teaching 1

---
+++ 01_firstFolder/one.js
@@ -0,0 +1,63 @@
+console.log("wasim")
+
+const accNo = 7847908787690;
+let accName = "SBI"
+var address = "Okhla, Delhi"
+photo = undefined;
+
+let isActive;
+
+console.log(accNo)
+console.log(accName)
+console.log(address)
+console.log(photo)
+console.log(isActive)
+
+console.table([accNo, accName, address, photo])
+
+
+
+let score = 100
+console.log(score++)
+console.log(score)
+
+let score1 = 500
+console.log(++score1)
+
+let score2 = 200;
+score2++
+console.log(score2)
+
+console.log(2>5)
+console.log("10" > 5)
+console.log("05" > 3)
+
+
+console.log(null > 0)
+console.log(null < 0)
+console.log(null >= 0)
+console.log(null <= 0)
+console.log(null == 0)
+console.log(null === 0)
+
+
+console.log(undefined == 0)
+console.log(undefined === 0)
+console.log(undefined > 0)
+console.log(undefined <0)
+console.log(undefined >= 0)
+console.log(undefined <= 0)
+
+
+const score3  = 100
+const score4 = 100.99
+const score5 = false;
+const score6 = null;
+
+console.table([score3, score4, score5, score6])
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId)
+
