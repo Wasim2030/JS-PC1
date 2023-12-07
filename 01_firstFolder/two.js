c56323365ff20c8ca4a5c86b7c1abda59e273a38
Wasim2030 <137741061+Wasim2030@users.noreply.github.com>
2023-12-07 07:53:54 +0000

    practice practice practice

---
+++ 01_firstFolder/two.js
@@ -0,0 +1,111 @@
+let valueInNum = Number(123);
+
+console.log(valueInNum);
+console.log(typeof valueInNum);
+console.log(typeof valueInNum);
+
+let score = "wasim";
+
+let scoreInNum = Number(score);
+console.log(scoreInNum);
+console.log(typeof scoreInNum);
+
+let bool = true;
+let boolToNum = Number(bool);
+console.log(typeof bool);
+
+console.log(boolToNum);
+
+let aNum = 1;
+console.log(Boolean(aNum));
+
+let bNum = 98;
+console.log(Boolean(bNum));
+
+let cNum = 0;
+console.log(Boolean(cNum));
+
+console.log(Boolean(10));
+
+console.log(Boolean("wasim"));
+
+let isLoggedIn = "wasim";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+console.log(Number(true));
+console.log(Number(false));
+
+console.log(Boolean(1));
+console.log(Boolean(0));
+console.log(Boolean(1111));
+console.log(Boolean(100000));
+console.log(Boolean(""));
+console.log(Boolean("wasim"));
+
+let fNum = 123123;
+
+let aStr = String(fNum);
+
+console.log(aStr);
+console.log(typeof aStr);
+
+let bool1 = false;
+console.log(String(bool1));
+console.log(typeof String(bool1));
+
+let value = 9;
+let negValue = -value;
+
+console.log(negValue);
+console.log(negValue - 9);
+
+console.log(2 + 7);
+console.log(2 - 7);
+console.log(2 * 7);
+console.log(2 / 7);
+console.log(3 % 1);
+
+const fName = "wasim";
+const lName = "akhter";
+
+const fullName = fName + lName;
+
+console.log(fullName);
+
+console.log(fName + " " + lName);
+
+console.log("2" + 2);
+console.log(2 + "2");
+
+console.log("1" + 2 + 2);
+console.log(2 + 2 + "3");
+
+console.log(true);
+console.log(+true);
+console.log(+true + 3);
+
+console.log(false);
+console.log(+false);
+console.log(+false + 3);
+
+console.log(+"");
+
+let num1 = (num2 = num3 = 2 + 2);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+let marioScore = 100;
+console.log(marioScore);
+console.log(marioScore++);
+marioScore++;
+console.log(marioScore);
+
+let marioScore2 = 50;
+console.log(++marioScore2);
+
+marioScore2++;
+console.log(marioScore2);
+console.log(marioScore2);
