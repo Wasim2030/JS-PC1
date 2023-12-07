915a493b58fcb5dd12df458abaf061a73a47ad54
Wasim2030 <137741061+Wasim2030@users.noreply.github.com>
2023-12-07 15:29:23 +0000

    practice lets us deeply understand things

---
+++ 02_secondFolder/03_maths.js
@@ -0,0 +1,29 @@
+console.log(Math);
+
+console.log(Math.abs(-99));
+console.log(Math.abs(999));
+
+console.log(Math.round(5.6));
+console.log(Math.round(5.4));
+console.log(Math.round(5.5));
+
+console.log(Math.ceil(9.1));
+console.log(Math.floor(9.9));
+
+console.log(Math.sqrt(4));
+console.log(Math.sqrt(5));
+
+console.log(Math.min(333, 98, 32, 89, 11, 98));
+console.log(Math.max(333, 98, 32, 89, 11, 98));
+
+console.log(Math.pow(3, 2));
+
+console.log(Math.random());
+console.log(Math.random() * 5);
+console.log(Math.random() * 5 + 1);
+console.log(Math.floor(Math.random() * 5 + 1));
+
+const max = 10;
+const min = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
