8494112dd9555fffbd2fd2ee1c362d3ce8e7c9f6
Wasim2030 <137741061+Wasim2030@users.noreply.github.com>
2023-12-10 14:23:52 +0000

    switch statement

---
+++ 04_fourthFolder/02_switchStatement.js
@@ -0,0 +1,41 @@
+const month = "april";
+
+switch (month) {
+  case "january":
+    console.log("its january");
+    break;
+  case "february":
+    console.log("its february");
+    break;
+  case "march":
+    console.log("its march");
+    break;
+  case "april":
+    console.log("its april");
+    break;
+  case "may":
+    console.log("its May");
+    break;
+  default:
+    console.log("its default");
+    break;
+}
+
+console.log("wasim");
+
+const day = "tuesday";
+
+switch (day) {
+  case "monday":
+    console.log("its monday");
+    break;
+  case "tuesday":
+    console.log("its tuesday");
+    break;
+  case "wednesday":
+    console.log("its wednesday");
+    break;
+  default:
+    console.log("its defauly");
+    break;
+}
